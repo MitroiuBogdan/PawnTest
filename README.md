624cedd6faf1e8d46879ebc43a8ddaf5a8c4b4d2
bogdan.mitroiu <mitroiu.bogdan@gmail.com>
2023-01-02 19:03:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ProjectReshape
+
+Developed with Unreal Engine 5
---
+++ Source/ProjectReshape/ProjectReshape.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectReshape.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectReshape, "ProjectReshape" );
