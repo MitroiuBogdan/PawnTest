3290c3c800497a24fa118891adae61ed26cea3a2
bogdan.mitroiu <mitroiu.bogdan@gmail.com>
2023-01-02 20:21:35 +0200

    Format strings

--- Source/ProjectReshape/Private/Items/Item2.cpp
+++ Source/ProjectReshape/Private/Items/Item2.cpp
@@ -6,22 +6,27 @@
 // Sets default values
 AItem2::AItem2()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
 void AItem2::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 60.f, FColor::Blue, FString("Hello from debug"));
+	}
 }
 
 // Called every frame
 void AItem2::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	if (GEngine)
+	{
+		FString message = FString::Printf(TEXT("dt -> %f"), DeltaTime);
+		GEngine->AddOnScreenDebugMessage(2, 60.f, FColor::Blue, message);
+	}
 }
-
